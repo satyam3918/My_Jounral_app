d0b78ba4c876647ca79cd81d4ed5f0ba2d3f3e6f
Satyam3918 <satyam3918@gmail.com>
2023-01-19 12:56:42 +0530

    looking for responce in postman dated 1/18 5:00

---
+++ springbootRestfulWebService/src/main/java/com/example/aspringbootRestfulWebService/AaSpringbokRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aspringbootRestfulWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaSpringbokRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaSpringbokRestfulWebServiceApplication.class, args);
+	}
+
+}
